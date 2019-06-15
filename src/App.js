ae9f089daecefbafc833c119645cecc44de4ffe9
akshaykatale99 <akshaykatale99@gmail.com>
2019-06-16 02:51:23 +0530

    get method changes

--- src/App.js
+++ src/App.js
@@ -1,68 +1,66 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import { Link } from "react-router-dom";
-import Loader from './components/loader';
+import Loader from "./components/loader";
 
-import ListView from './components/list';
-import ListItem from './components/listItem';
-
+import ListView from "./components/list";
+import ListItem from "./components/listItem";
 
 class App extends React.Component {
- constructor(props) {
-   super(props);
- 
-   this.state = {
-     data: []
-   };
- }
+  constructor(props) {
+    super(props);
 
- getData() {
+    this.state = {
+      data: []
+    };
+  }
 
-      const options = {
-          method: 'GET',
-      };
+  getData() {
+    this.setState({ loading: true });
+    const options = {
+      method: "GET"
+    };
 
-      return fetch('https://jsonplaceholder.typicode.com/posts', options)
-      .then((response) => response.json())
-      .then((responseJson) => { 
-          return responseJson;
+    fetch("https://jsonplaceholder.typicode.com/posts", options)
+      .then(response => response.json())
+      .then(responseJson => {
+        this.setState({ loading: false });
+        this.setState({ data: responseJson });
       })
-      .catch((error) => {
-          console.log(error);
-          return error;
+      .catch(error => {
+        console.log(error);
       });
   }
 
+  componentDidMount() {
+    this.getData();
+  }
 
- componentDidMount(){
-   this.setState({loading: true});
-   this.getData().then(res => {
-     this.setState({loading: false});
-     this.setState({data: res});
-   });
- }
-
- render(){
+  render() {
     return (
       <div className="App">
-          { (this.state.loading) ? <Loader/> : ''}
-          <ListView
-              cssClass = "list" 
-              data = {this.state.data}
-              listHeader = { 
-                <div className="list-header">
-                  <h3>Posts List</h3>
-                  <Link className="btn btn-primary" to="/single/new"> Add </Link>
-                </div>
-              }
-              itemToRender = {(item) => <ListItem
-                                          data={item}
-                                          key={item.id} 
-                                          onView = {(id) => this.props.history.push("/single/view/" + id)}
-                                          onEdit = {(id) => this.props.history.push("/single/edit/" + id)} 
-                                          />
-                                      }
-           />
+        {this.state.loading ? <Loader /> : ""}
+        <ListView
+          cssClass="list"
+          data={this.state.data}
+          listHeader={
+            <div className="list-header">
+              <h3>Posts List</h3>
+              <Link className="btn btn-primary" to="/post/new">
+                {" "}
+                Add{" "}
+              </Link>
+            </div>
+          }
+          itemToRender={item => (
+            <ListItem
+              data={item}
+              key={item.id}
+              onView={id => this.props.history.push("/post/view/" + id)}
+              onEdit={id => this.props.history.push("/post/edit/" + id)}
+            />
+          )}
+        />
       </div>
     );
   }
